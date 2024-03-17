774afe37096fa74c1afaec74a6c277f3b6c66481
liamdtodd <91576595+liamdtodd@users.noreply.github.com>
2024-03-17 12:15:05 -0400

    add route, global styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { css, Global } from '@emotion/react'
 
 import Root from './pages/Root'
 import APOD from './pages/APOD'
@@ -11,13 +12,26 @@ import Earth from './pages/Earth'
 import App from './App'
 
 const queryClient = new QueryClient()
+
+const globalStyles = css`
+    @import url('https://fonts.googleapis.com/css2?family=Linden+Hill:ital@0;1&display=swap');
+    html {
+        font-family: 'Linden Hill', serif;
+    }
+    body {
+        margin: 0px;
+        padding: 0px;
+    }
+`
+
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root />,
         children: [
-            { path: "APOD", element: <APOD /> },     // For APOD queries
-            { path: "Earth", element: <Earth /> },     // For Earth landsat queries
+            { path: "home", element: <App /> },
+            { path: "apod", element: <APOD /> },     // For APOD queries
+            { path: "earth", element: <Earth /> },     // For Earth landsat queries
             //{ path: "*", element: <ErrorPage /> },  // Catches all other paths 
         ]
     }
@@ -25,6 +39,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <Global styles={globalStyles} />
         <QueryClientProvider client={queryClient}>
             <RouterProvider router={router} />
         </QueryClientProvider>
