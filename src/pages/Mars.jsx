393977aa12f215029b709055a4643380b36b19c4
liamdtodd <91576595+liamdtodd@users.noreply.github.com>
2024-03-17 23:21:52 -0400

    styling and display of data for Mars

--- src/pages/Mars.jsx
+++ src/pages/Mars.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 
+import Spinner from "../Spinner";
+import ErrorPage from "./ErrorPage";
+
 const API_KEY = 'RcxPtX3fOecovjtNyb6h50kQbYp1gBcEgiiYr3TG';
 
 export default function Mars() {
@@ -22,32 +25,110 @@ export default function Mars() {
         }
     });
 
-    return (
-        <>
-            <div>
-                <h1>Mars Rover Photos</h1>
-            </div>
+    const outerDivStyles = css`
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-around;
 
-            <div>
-                <form onSubmit={(e) => {
+        height: 85vh;
+        width: 100vw;
+    `
+
+    const innerDivStyles = css`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+
+        background-color: rgba(255,255,255,0.75);
+        border: 3px solid white;
+        border-radius: 20px;
+
+        height: 75vh;
+        width: 45vw;
+    `
+
+    const imgStyles = css`
+        display: block;
+        margin: auto;
+        max-height: 60vh;
+        max-width: 40vw;
+    `
+
+    const formStyles = css`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        text-align: center;
+
+        select {
+            margin-bottom: 10vh;
+        }
+
+        button {
+            background-color: rgba(45,45,45,1);
+            color: white;
+            border: 2px solid white;
+            border-radius: 20px;
+
+            margin-top: 15vh;
+
+            height: 7vh;
+            width: 15vw;
+
+            &:hover {
+                background-color: rgba(70,70,70,1);
+                color: red;
+                border: 2px solid red;
+                cursor: pointer;
+            }
+        }
+
+        select, input {
+            width: 15vw;
+            height: 3vh;
+            margin-right: 10px;
+    
+            border-radius: 10px;
+        }
+    `
+
+    return (
+        <div css={outerDivStyles}>
+            <div css={innerDivStyles}>
+                <h1>Mars Rover Photos</h1>
+
+                <form css={formStyles} onSubmit={(e) => {
                     e.preventDefault();
                     setSearchParams({ sol: solQuery, camera: cameraQuery});
                 }}>
-                    <label for="cameras">Choose a camera</label>
+                    <label for="cameras"><h3>Choose a camera</h3></label>
                     <select id="cameras" name="cameras" onChange={(e) => setCameraQuery(e.target.value)}>
                         <option value="fhaz">Front Hazard Avoidance Camera</option>
-                        <option value={cameraQuery}>Rear Hazard Avoidance Camera</option>
-                        <option value={cameraQuery}>Mast Camera</option>
-                        <option value={cameraQuery}>Chemistry and Camera Complex</option>
-                        <option value={cameraQuery}>Mars Hand Lens Imager</option>
-                        <option value={cameraQuery}>Mars Descent Imager</option>
-                        <option value={cameraQuery}>Navigation Camera</option>
+                        <option value="rhaz">Rear Hazard Avoidance Camera</option>
+                        <option value="mast">Mast Camera</option>
+                        <option value="chemcam">Chemistry and Camera Complex</option>
+                        <option value="mahli">Mars Hand Lens Imager</option>
+                        <option value="mardi">Mars Descent Imager</option>
+                        <option value="navcam">Navigation Camera</option>
                     </select>
 
+                    <h3>Enter a Martian Sol</h3>
                     <input value={solQuery} onChange={(e) => setSolQuery(e.target.value)} />
                     <button type="submit">Submit Data</button>
                 </form>
             </div>
-        </>
+            
+            <div css={innerDivStyles}>
+                {isLoading && <Spinner />}
+                {error && <ErrorPage />}
+                {data?.photos && data.photos.length > 0 && (
+                    <img src={data.photos[0].img_src} alt="Mars Rover photo" css={imgStyles}/>
+                )}
+            </div>
+        </div>
     )
 }
